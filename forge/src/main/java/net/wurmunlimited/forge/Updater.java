e3204c009fb96d4900a38b3ad17f5a0afa8b4d98
Per Löwgren <per.lowgren@gmail.com>
2020-10-11 01:33:34 +0200

    Create forge module, which will handle certain processes such as updating mods.

--- forge/src/main/java/net/wurmunlimited/forge/Updater.java
+++ forge/src/main/java/net/wurmunlimited/forge/Updater.java
@@ -1,17 +1,29 @@
 package net.wurmunlimited.forge;
 
+import net.wurmunlimited.forge.config.ForgeConfig;
+import net.wurmunlimited.forge.util.FileUtil;
+
 import javax.swing.*;
 import java.awt.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 
-public class AutoInstaller {
+public class Updater {
 
-    private static final Logger logger = Logger.getLogger(AutoInstaller.class.getName());
+    private static final Logger logger = Logger.getLogger(Updater.class.getName());
 
     public static void main(final String[] args) {
-        ServerConnection.getInstance().getAvailableMods(args[1]);
-        if(ServerConnection.getInstance().install()) {
+        Updater updater = new Updater();
+    }
+
+    private Updater() {
+        Path baseDir = Paths.get("").getParent();
+        Properties properties = FileUtil.loadProperties("forge.properties");
+        ForgeConfig.init(baseDir,properties);
+        if(VersionHandler.getInstance().install()) {
             JPanel panel = new JPanel();
             panel.setLayout(new GridLayout(2,1));
             panel.setBounds(0,0,468,120);
